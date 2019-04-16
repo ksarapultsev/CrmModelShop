c3a07823f59c92fb5e979bed7411ded0ec53ceea
ksarapultsev <ksarapultsev2014@gmail.com>
2019-04-16 23:08:25 +0600

    Day 1

---
+++ CrmUi/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrmBl.Model;
+
+namespace CrmUi
+{
+    public partial class Main : Form
+    {
+        CrmContext db;
+       
+        public Main()
+        {
+            InitializeComponent();
+            db = new CrmContext();
+        }
+
+        private void ProductToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             var catalogProduct = new Catalog<Product>(db.Products);
+             catalogProduct.Show();
+        }
+
+        private void SellerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var catalogSeller = new Catalog<Seller>(db.Sellers);
+            catalogSeller.Show();
+        }
+
+        private void CustomerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var catalogCustomer = new Catalog<Customer>(db.Customers);
+            catalogCustomer.Show();
+        }
+
+        private void CheckToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var CheckCustomer = new Catalog<Check>(db.Checks);
+            CheckCustomer.Show();
+        }
+
+        private void CustomerAddToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            var form = new CustomerForm();
+            if(form.ShowDialog() == DialogResult.OK)
+            {
+                db.Customers.Add(form.Customer);
+                db.SaveChanges();
+            }
+        }
+    }
+}
